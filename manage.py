c2d4b476ff0f4eb7833009468f93858172cebde0
Tristin <tmille2004@gmail.com>
2023-01-14 19:23:16 -0600

    removes main app data"

--- manage.py
+++ manage.py
@@ -6,7 +6,11 @@ import sys
 
 def main():
     """Run administrative tasks."""
+<<<<<<< HEAD
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
+=======
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stocktrackapp.settings')
+>>>>>>> 68bccf09 (renames structure)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
